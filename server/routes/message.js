9f9276c7b7eb55e46c2fa4763131515927673e99
Isaac Jeddiah <isaacjeddiah@gmail.com>
2025-06-22 22:53:11 +0530

    updated backend for messaging:test 7

--- server/routes/message.js
+++ server/routes/message.js
@@ -6,10 +6,10 @@ const {
   getMessages,
   getFriendsForMessaging,
 } = require("../controllers/message");
-const auth = require("../middlewares/auth");
+const {authUser} = require("../middlewares/auth");
 
-router.get("/friends", auth, getFriendsForMessaging); // List of friends for chat
-router.get("messages/getmessage/:id", auth, getMessages); // Get message history with user
-router.post("messages/sendmessage/:id", auth, sendMessage); // Send message to user
+router.get("/friends", authUser, getFriendsForMessaging); // List of friends for chat
+router.get("messages/getmessage/:id", authUser, getMessages); // Get message history with user
+router.post("messages/sendmessage/:id", authUser, sendMessage); // Send message to user
 
 module.exports = router;
